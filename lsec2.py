736c39b61c90f655d4c0f6583b6709422c6d279c
Scott Cunningham <scott.cunningham@ensighten.com>
2015-09-21 12:37:26 +0100

    lsec2: match based on regex

--- lsec2.py
+++ lsec2.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python
 import boto.ec2
+import re
 import sys
 
 '''
@@ -7,12 +8,13 @@ USAGE: lsec2 [filter]
     prints all instances (optionally filtered by name by the `filter` param) and their id, Name, instance type, and DNS.
 '''
 
-def get_ec2_instances(region, name_filter=""):
+def get_ec2_instances(region, name_filter=".*"):
     ec2_conn = boto.ec2.connect_to_region(region)
     instances = ec2_conn.get_all_instances()
+    regex = re.compile(name_filter)
     for instance in instances:
         i = instance.instances[0]
-        if name_filter in i.tags.get('Name', '') and i.state != 'terminated':
+        if regex.match(i.tags.get('Name', '')) and i.state != 'terminated':
             print "{:<10} {:<40} {:<10} {:<20}".format(i.id, i.tags.get('Name', ''), i.instance_type,
                                                        i.dns_name or "No DNS name")
 
